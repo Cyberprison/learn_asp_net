6375145d9c9d451d0e67c331f7a2cadcc62a99b0
whoami <az.khabibullin2018@gmail.com>
2024-06-11 21:44:43 +0500

    урок проходил в одном контроллере
    добавил два простых контроллера для вывода данных в представления
    добавил контроллер который вычисляет сумму, однако параметры передаются только по URL

--- learn_aspdotnet/Controllers/HomeController.cs
+++ learn_aspdotnet/Controllers/HomeController.cs
@@ -6,8 +6,52 @@ using System.Web.Mvc;
 
 namespace learn_aspdotnet.Controllers
 {
+    //class User
+    //{
+    //    public int Age { get; set; }
+    //    public string Name { get; set; }
+    //}
+
     public class HomeController : Controller
     {
+        
+
+        public ActionResult PrintValue()
+        {
+            int age = 16;
+            string name = "azazaza";
+            //var user = new User
+            //{
+            //    Name = name,
+            //    Age = age
+            //};
+
+            return View(name);
+
+        }
+
+        public ActionResult PrintValuesCollection()
+        {
+            var numbersList = new List<int>{ 0,1,2,3,4,5,6,7,8,9};
+            var numbersArray = new string[] { "1", "2", "3" };
+            //var users = new List<User>
+            //{
+            //    new User {Name = "aaa", Age = "111"},
+            //    new User {Name = "bbb", Age = "222"},
+            //    new User {Name = "ccc", Age = "333"}
+            //};
+
+            return View(numbersList);
+        }
+
+        public ActionResult Calculate(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber + secondNumber;
+
+            return View(result);
+        }
+
+
         public ActionResult PrintInfo()
         {
             return View();
