4056cf5637cc573e149a6848e6ede01af75cb5f3
whoami <az.khabibullin2018@gmail.com>
2024-06-12 16:18:50 +0500

    Изучил хтмл хелперы Создал папку Helpers и класс ListHelper, где буду писать первые хтмл хелперы. создал новый контроллер ProductController, метод GetAllProducts и представление для него. Куда поместил первый хелпер, который просто создает заголовок х2
    не забудь подключить пространство имен, где хранится хелпер, в представление

---
+++ learn_aspdotnet/Helpers/ListHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc; //for IHtmlHelper
+
+namespace learn_aspdotnet.Helpers
+{
+    //модификатор статик чтобы не создавать экземпляр класса
+    public static class ListHelper
+    {
+        public static HtmlString CreateParagraph(this HtmlHelper html, string text)
+        {
+            var result = $"<h2>{text}</h2>";
+            return new HtmlString(result);
+        }
+
+
+
+
+    }
+}
